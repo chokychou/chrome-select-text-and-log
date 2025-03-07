7af07caa88a1323731abb611babaef4557f1841f
chokychou <nobreakfast@foxmail.com>
2025-03-07 01:41:31 -0800

    clean up

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(async (request) => {
-  if (request.message == 'mouseup') {
+  if (request.message == 'selectionchange') {
     console.log(request.text);
   }
 });
